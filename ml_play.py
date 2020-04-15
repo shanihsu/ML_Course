cce6efcf36c3cfa37b05dcf6a1061d761d14165b
shanihsu <shani21516344@gmail.com>
2020-04-16 02:12:39 +0800

    commit

--- ml_play.py
+++ ml_play.py
@@ -6,6 +6,19 @@ import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
 )
+"""
+def predict(x, l_x, y, l_y):
+    m = (y - l_y)/(x - l_x)
+    predict_x = (395 - y)/m + x
+    for i in range(10):
+        if (predict_x > 195):
+            predict_x = 390 - predict_x
+        elif (predict_x < 0):
+            predict_x = 0 - predict_x
+        else:
+            break
+    return predict_x
+"""
 
 def ml_loop():
     """
@@ -22,7 +35,12 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-
+    last_x = 75
+    last_y = 400
+    predict_x = 75
+    predict_y = 401
+    direction = 0
+    # 0:leftup, 1:rightup, 2:leftdown, 3:rightdown
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
 
@@ -43,40 +61,82 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-
+        (ball_x,ball_y) = scene_info.ball
+        platform_x = scene_info.platform[0]
+        platform_y = scene_info.platform[1]
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
+            if((ball_x - last_x) < 0 and (ball_y - last_y) < 0):
+                direction = 0
+            elif((ball_x - last_x) > 0 and (ball_y - last_y) < 0):
+                direction = 1
+            elif((ball_x - last_x) < 0 and (ball_y - last_y) > 0):
+                direction = 2
+            elif((ball_x - last_x) > 0 and (ball_y - last_y) > 0):
+                direction = 3
+            m = (ball_y - last_y) / (ball_x - last_x)
+            for i in range(30):
+                if(direction == 0):
+                    predict_x = 0
+                    predict_y = m * (0 - ball_x) + ball_y
+                    direction = 1
+                    if(predict_y < 0):
+                        predict_x = (0 - ball_y) / m + ball_x
+                        predict_y = 0
+                        direction = 2
+                elif(direction == 1):
+                    predict_x = 195
+                    predict_y = m * (195 - ball_x) + ball_y
+                    direction = 0
+                    if(predict_y < 0):
+                        predict_x = (0 - ball_y) / m + ball_x
+                        predict_y = 0
+                        direction = 3
+                elif(direction == 2):
+                    predict_x = 0
+                    predict_y = m * (0 - ball_x) + ball_y
+                    direction = 3
+                    if(predict_y > 395):
+                        predict_x = (400 - ball_y) / m + ball_x
+                        predict_y = 400
+                        direction = 0
+                        break
+                elif(direction == 3):
+                    predict_x = 195
+                    predict_y = m * (195 - ball_x) + ball_y
+                    direction = 2
+                    if(predict_y > 395):
+                        predict_x = (400 - ball_y) / m + ball_x
+                        predict_y = 400
+                        direction = 1
+                        break
+                m = -m
             """
-            ball_x = scene_info.ball[0]
-            ball_y = scene_info.ball[1]
-            platform_x = scene_info.platform[0]
-            platform_y = scene_info.platform[1]
-            ball_next = scene_info.ball[0]
-            ball_hit = False
-            if ball_x == 0 and ball_y >= 200:
-                ball_next = 0 + platform_y - ball_y
-                ball_hit = True
-            elif ball_x == 200 and ball_y >= 200:
-                ball_next = 200 - (platform_y - ball_y)
-                ball_hit = True
-            if ball_next > platform_x and ball_hit == True:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif ball_next < platform_x and ball_hit == True:
+            if(predict_x < platform_x + 20):
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            else :
+            elif(predict_x > platform_x + 20):
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            else:
                 comm.send_instruction(scene_info.frame, PlatformAction.NONE)
             """
-            ball_x = scene_info.ball[0]
-            platform_x = scene_info.platform[0]
-            if ball_x > platform_x :
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif ball_x < platform_x :
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            else :
-                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
-            
-            
-            
+            if (last_y - ball_y) < 0:
+                    #x = predict(ball_x,last_x,ball_y,last_y)
+                    if predict_x < (platform_x + 20):
+                        comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                    elif predict_x > (platform_x + 25):
+                        comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                    else:
+                        comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            else:
+                    if ball_x < (platform_x + 20):
+                        comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                    elif ball_x > (platform_x + 25):
+                        comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                    else:
+                        comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+              
+            last_x = ball_x
+            last_y = ball_y
